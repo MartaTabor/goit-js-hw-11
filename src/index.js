8bf46c57e32652d7b51a6f83fb325a8e3b9ea825
Marta Tabor <leeloothefifth@googlemail.com>
2024-02-19 20:28:52 +0000

    params add

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 import axios from 'axios';
+
+const searchParams = new URLSearchParams({
+  key: '42459291-7f50c47c6b19e5b61fce58d70',
+  q: 'query',
+  image_type: 'photo',
+  orientation: 'horizontal',
+  safesearch: 'true',
+  page: 1,
+  per_page: 40,
+});
+
+const fetchPhotos = axios
+  .get(`https://pixabay.com/api/${searchParams}`)
+  .then(response => response.data);

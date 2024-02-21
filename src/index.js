9ddcf2a1d7f08da2ba8570a489d0e755817ffc74
Marta Tabor <leeloothefifth@googlemail.com>
2024-02-21 21:41:16 +0000

    first tries

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import axios from 'axios';
+import Notiflix from 'notiflix';
+
+const searchQuery = 'dog';
+const gallery = document.querySelector('.gallery');
 
 const searchParams = new URLSearchParams({
   key: '42459291-7f50c47c6b19e5b61fce58d70',
-  q: 'query',
+  q: searchQuery.split(' ').join('+'),
   image_type: 'photo',
   orientation: 'horizontal',
   safesearch: 'true',
@@ -10,6 +14,38 @@ const searchParams = new URLSearchParams({
   per_page: 40,
 });
 
-const fetchPhotos = axios
-  .get(`https://pixabay.com/api/${searchParams}`)
-  .then(response => response.data);
+const fetchPhotos = async () => {
+  const searchResults = await axios.get(
+    `https://pixabay.com/api/?${searchParams}`
+  );
+  return searchResults.data;
+};
+
+fetchPhotos()
+  .then(searchResults => console.log(searchResults.data))
+  .catch(error => console.log(error));
+
+// function renderPhotos(searchResults) {
+//   const markup = searchResults
+//     .map(
+//       hit => `<div class="photo-card">
+//   <img src="${hit.webformatURL}" alt="${hit.tags}" loading="lazy" />
+//   <div class="info">
+//     <p class="info-item">
+//       <b>Likes: ${hit.likes}</b>
+//     </p>
+//     <p class="info-item">
+//       <b>Views: ${hit.views}</b>
+//     </p>
+//     <p class="info-item">
+//       <b>Comments: ${hit.comments}</b>
+//     </p>
+//     <p class="info-item">
+//       <b>Downloads: ${hit.downloads}</b>
+//     </p>
+//   </div>
+// </div>`
+//     )
+//     .join('');
+//   gallery.innerHTML = markup;
+// }

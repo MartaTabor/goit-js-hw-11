a39242eb0c1c533ae009cf074a0553e3a9af189c
Marta Tabor <leeloothefifth@googlemail.com>
2024-02-24 16:21:15 +0000

    render photos add

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 import axios from 'axios';
 import Notiflix from 'notiflix';
 
-const searchQuery = 'dog';
+const searchQuery = document.querySelector('#search-form');
 const gallery = document.querySelector('.gallery');
+const searchBtn = document.querySelector('button');
+
+searchBtn.addEventListener('click', async event => {
+  event.preventDefault();
+  try {
+    const photos = await fetchPhotos();
+    renderPhotos(photos);
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 const searchParams = new URLSearchParams({
   key: '42459291-7f50c47c6b19e5b61fce58d70',
-  q: searchQuery.split(' ').join('+'),
+  q: '',
   image_type: 'photo',
   orientation: 'horizontal',
   safesearch: 'true',
@@ -15,45 +26,18 @@ const searchParams = new URLSearchParams({
 });
 
 const fetchPhotos = async () => {
+  searchParams.set('q', searchQuery.elements[0].value.split(' ').join('+'));
   const searchResults = await axios.get(
     `https://pixabay.com/api/?${searchParams}`
   );
-  const searchData = await searchResults.data;
-  return searchData;
+  return searchResults.data;
 };
 
-fetchPhotos()
-  // .then(searchData => console.log(searchData))
-  .then(({ hits }) => {
-    const markup = hits
-      .map(
-        hit => `<div class="photo-card">
-  <img src="${hit.webformatURL}" alt="${hit.tags}" loading="lazy" />
-  <div class="info">
-    <p class="info-item">
-      <b>Likes: ${hit.likes}</b>
-    </p>
-    <p class="info-item">
-      <b>Views: ${hit.views}</b>
-    </p>
-    <p class="info-item">
-      <b>Comments: ${hit.comments}</b>
-    </p>
-    <p class="info-item">
-      <b>Downloads: ${hit.downloads}</b>
-    </p>
-  </div>
-</div>`
-      )
-      .join('');
-    gallery.innerHTML = markup;
-  })
-  .catch(error => console.log(error));
-
-// function renderPhotos(searchData) {
-//   const markup = searchData
-//     .map(
-//       hit => `<div class="photo-card">
+// fetchPhotos()
+//   .then(({ hits }) => {
+//     const markup = hits
+//       .map(
+//         hit => `<div class="photo-card">
 //   <img src="${hit.webformatURL}" alt="${hit.tags}" loading="lazy" />
 //   <div class="info">
 //     <p class="info-item">
@@ -70,9 +54,46 @@ fetchPhotos()
 //     </p>
 //   </div>
 // </div>`
-//     )
-//     .join('');
-//   gallery.innerHTML = markup;
-// }
+//       )
+//       .join('');
+//     gallery.innerHTML = markup;
+//   })
+//   .catch(error => console.log(error));
 
-// renderPhotos();
+function renderPhotos(data) {
+  if (data.hits.length <= 0) {
+    Notiflix.Notify.failure(
+      'Sorry, there are no images matching your search query. Please try again.'
+    );
+  } else {
+    const markup = data.hits
+      .map(
+        ({
+          webformatURL,
+          tags,
+          likes,
+          views,
+          comments,
+          downloads,
+        }) => `<div class="photo-card">
+  <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+  <div class="info">
+    <p class="info-item">
+      <b>Likes: ${likes}</b>
+    </p>
+    <p class="info-item">
+      <b>Views: ${views}</b>
+    </p>
+    <p class="info-item">
+      <b>Comments: ${comments}</b>
+    </p>
+    <p class="info-item">
+      <b>Downloads: ${downloads}</b>
+    </p>
+  </div>
+</div>`
+      )
+      .join('');
+    gallery.innerHTML = markup;
+  }
+}
